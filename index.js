79f53977ff0db3a29a3fa75d9100a0d11b88e107
AaJii73 <antti-jussi.miettinen@aalto.fi>
2023-11-23 16:08:53 +0200

    3.8 Phonebook backend step8

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
-  
+
+morgan.token('post-body', function (req, res) { return req.method ==="POST" ? JSON.stringify(req.body) : "" })
+app.use( morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
 let persons = [
     { 
