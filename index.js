ead5fab724197f0ddd80015fdafe114d9485790f
AaJii73 <antti-jussi.miettinen@aalto.fi>
2023-11-23 15:00:23 +0200

    3.6 Phonebook backend step6

--- index.js
+++ index.js
@@ -57,6 +57,25 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {   
     const body = request.body
 
+    if (!body.name)
+    {
+        return response.status(400).json({ 
+            error: 'name missing' 
+        })
+    }
+    if (persons.some(person => person.name === body.name))
+    {
+        return response.status(400).json({ 
+            error: 'person already added to phonebook' 
+        })
+    }
+    if (!body.number)
+    {
+        return response.status(400).json({ 
+            error: 'number missing' 
+        })
+    }
+
     const person = {
       name: body.name,
       number: body.number,
